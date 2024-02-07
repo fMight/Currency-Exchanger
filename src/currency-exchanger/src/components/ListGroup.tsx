2a6a7954a06836589c2800ad08b626d3a0741791
firat <firat.ahmet.dogan@gmail.com>
2024-02-07 16:19:10 +0100

    - started building the front page - created a sample list of the currencies that can be converted into another

---
+++ src/currency-exchanger/src/components/ListGroup.tsx
@@ -0,0 +1,24 @@
+import { MouseEvent } from "react";
+
+function ListGroup() {
+  let items = ["Euro", "Lira", "Dollar", "Pounds"];
+
+  //Event handler for the click event
+  const handleClick = (event: MouseEvent) => console.log(event);
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
